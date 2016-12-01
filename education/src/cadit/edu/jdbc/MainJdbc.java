c854de2396af93af11de34879556080f4245da1d
CADIT\riva <nicola.riva@caditgroup.com>
2016-12-01 11:46:34 +0100

    Passaggio al database H2

--- education/src/cadit/edu/jdbc/MainJdbc.java
+++ education/src/cadit/edu/jdbc/MainJdbc.java
@@ -1,89 +1,114 @@
 package cadit.edu.jdbc;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+// H2 In-Memory Database Example shows about storing the database contents into memory. 
 
 public class MainJdbc {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
-		try {
-			//Load JDBC Driver
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+    private static final String DB_DRIVER = "org.h2.Driver";
+    private static final String DB_CONNECTION = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+    private static final String DB_USER = "";
+    private static final String DB_PASSWORD = "";
 
-			//Open connection
-			Connection con = DriverManager.getConnection("jdbc:sqlserver://localhost;instanceName=sqlexpress;databaseName=Edu","edu","edu");
-			checkForWarning (con.getWarnings());
+    public static void main(String[] args) throws Exception {
+        try {
+            insertWithStatement();
+            insertWithPreparedStatement();
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
-			//Create statement object and execute it
-			Statement stmt=con.createStatement();
-			String sel = "SELECT a 'First',b,c FROM table1";
-			ResultSet rs = stmt.executeQuery(sel);
-			
-			//Use ResultSetMetaData
-			ResultSetMetaData md = rs.getMetaData();
+    private static void insertWithPreparedStatement() throws SQLException {
+        Connection connection = getDBConnection();
+        PreparedStatement createPreparedStatement = null;
+        PreparedStatement insertPreparedStatement = null;
+        PreparedStatement selectPreparedStatement = null;
 
-			//Get number of column
-			int nc = md.getColumnCount();
+        String CreateQuery = "CREATE TABLE PERSON(id int primary key, name varchar(255))";
+        String InsertQuery = "INSERT INTO PERSON" + "(id, name) values" + "(?,?)";
+        String SelectQuery = "select * from PERSON";
 
-			//Get column name
-			for (int j=1;j<=nc;j++) {
-				System.out.printf("%-11s",md.getColumnName(j));
-			}	
-			System.out.println();
+        try {
+            connection.setAutoCommit(false);
 
-			//Looping on rows
-			while (rs.next()) {
-				int a = rs.getInt("First");		// By Alias
-				float b = rs.getFloat("b");		// By Name
-				String c = rs.getString(3);		// By Index
-				
-				System.out.printf("%-10d %-10f %s\r\n", a, b, c);
-			}
-		
-			rs.close();
-			con.close();
-			
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			System.out.println("\r\nSQLException\r\n");
-			while ( e != null ) {
-				System.out.println("SQLState : " + e.getSQLState());
-				System.out.println("Messagge : " + e.getMessage());
-				System.out.println("Error : " + e.getErrorCode());
-				// Loop on exceptions
-				e = e.getNextException();
-				System.out.println();
-			}
-		} catch(Exception e)
-		{
-			e.printStackTrace();
-		}
+            createPreparedStatement = connection.prepareStatement(CreateQuery);
+            createPreparedStatement.executeUpdate();
+            createPreparedStatement.close();
 
-	}
-	
-	private static boolean checkForWarning(SQLWarning warn)  throws SQLException {
-		          
-		boolean rc = false;
+            insertPreparedStatement = connection.prepareStatement(InsertQuery);
+            insertPreparedStatement.setInt(1, 1);
+            insertPreparedStatement.setString(2, "Jose");
+            insertPreparedStatement.executeUpdate();
+            insertPreparedStatement.close();
 
-		if (warn != null) {
-			System.out.println("\n**Warning**\n");
-			rc = true;
-			while (warn != null ) {
-				System.out.println("SQLState : " + warn.getSQLState());
-				System.out.println("Message : " + warn.getMessage());
-				System.out.println("Error : " + warn.getErrorCode());
-				System.out.println("");
+            selectPreparedStatement = connection.prepareStatement(SelectQuery);
+            ResultSet rs = selectPreparedStatement.executeQuery();
+            System.out.println("H2 In-Memory Database inserted through PreparedStatement");
+            while (rs.next()) {
+                System.out.println("Id " + rs.getInt("id") + " Name " + rs.getString("name"));
+            }
+            selectPreparedStatement.close();
 
-				warn = warn.getNextWarning();
-			}
-		}
-		return rc;
-	}
-	
+            connection.commit();
+        } catch (SQLException e) {
+            System.out.println("Exception Message " + e.getLocalizedMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
+        }
+    }
 
+    private static void insertWithStatement() throws SQLException {
+        Connection connection = getDBConnection();
+        Statement stmt = null;
+        try {
+            connection.setAutoCommit(false);
+            stmt = connection.createStatement();
+            stmt.execute("CREATE TABLE PERSON(id int primary key, name varchar(255))");
+            stmt.execute("INSERT INTO PERSON(id, name) VALUES(1, 'Anju')");
+            stmt.execute("INSERT INTO PERSON(id, name) VALUES(2, 'Sonia')");
+            stmt.execute("INSERT INTO PERSON(id, name) VALUES(3, 'Asha')");
+
+            ResultSet rs = stmt.executeQuery("select * from PERSON");
+            System.out.println("H2 In-Memory Database inserted through Statement");
+            while (rs.next()) {
+                System.out.println("Id " + rs.getInt("id") + " Name " + rs.getString("name"));
+            }
+
+            stmt.execute("DROP TABLE PERSON");
+            stmt.close();
+            connection.commit();
+        } catch (SQLException e) {
+            System.out.println("Exception Message " + e.getLocalizedMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
+        }
+    }
+
+    private static Connection getDBConnection() {
+        Connection dbConnection = null;
+        try {
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+            return dbConnection;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return dbConnection;
+    }
 }
