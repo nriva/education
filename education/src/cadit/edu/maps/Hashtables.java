4185dd5a566d4663b1f7da7292c28dfe4fd41927
nicolariva <nicola.riva@caditgroup.com>
2019-07-26 11:55:15 +0200

    Nuovi esempi per le mappe

---
+++ education/src/cadit/edu/maps/Hashtables.java
@@ -0,0 +1,26 @@
+package cadit.edu.maps;
+
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Hashtables {
+
+	public static void main(String[] args) {
+
+		// Dalla versione 2 di java, Hashtable è diventata una map!
+		
+		Map<String, Long> table = new Hashtable<String, Long>();
+		
+		table.put("New York", (long) 8398748);
+		
+		for(Entry<String, Long> pair:table.entrySet())
+		{
+			System.out.println("The population of " + pair.getKey() + " is " + pair.getValue());
+		}
+		
+		table.put("Beijing", null);
+
+	}
+
+}
--- education/src/cadit/edu/maps/MainMaps.java
+++ education/src/cadit/edu/maps/MainMaps.java
@@ -1,18 +1,35 @@
 package cadit.edu.maps;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 
 public class MainMaps {
-
+	
+	private static String[] cities = new String[]  {"New York","Beijing", "Mumbai"};
+	private static long[] populations = new long[]  {8398748,21516000,12478447};
+	
 	public static void main(String[] args) {
-
-		Map<String,Long> population = createTreeMap();
+		System.out.println("HashMap Test:");
+		
+		Map<String,Long> population = createHashMap();
 		iterate(population);
 		
-
+		System.out.println("\r\nTreeMap Test:");
+		population = createTreeMap();
+		iterate(population);
+		
+		System.out.println("\r\nLinkedHashMap Test:");
+		population = createLinkedHashMap();
+		iterate(population);
+		
+		System.out.println("\r\nLinkedHashMap Test/2:");
+		iterate2((LinkedHashMap<String, Long>)population);
+		
+		population.put("Beijing", null);
 	}
 
 	private static void iterate(Map<String, Long> population) {
@@ -20,28 +37,47 @@ public class MainMaps {
 		{
 			System.out.println("The population of " + pair.getKey() + " is " + pair.getValue());
 		}
-
-		
 	}
+	
+	private static void iterate2(LinkedHashMap<String, Long> population) {
+		
+		Iterator<Entry<String, Long>> iterator = population.entrySet().iterator();
+		
+		while(iterator.hasNext())
+		{
+			Entry<String, Long> next = iterator.next();
+			System.out.println("The population of " + next.getKey() + " is " + next.getValue());
+		}
+	}
+	
 
 	private static Map<String, Long> createHashMap() {
 		HashMap<String, Long> hashMap = new HashMap<>();
 		
-		hashMap.put("New York",(long) 1000);
-		hashMap.put("Moscow",(long) 1000);
-		hashMap.put("Beijing",(long) 1000);
+		populateMap(hashMap);
 		
 		return hashMap;
 	}
 	
 	private static Map<String, Long> createTreeMap() {
-		TreeMap<String, Long> hashMap = new TreeMap<>();
+		TreeMap<String, Long> treeMap = new TreeMap<>();
 		
-		hashMap.put("New York",(long) 1000);
-		hashMap.put("Moscow",(long) 1000);
-		hashMap.put("Beijing",(long) 1000);
+		populateMap(treeMap);
 		
-		return hashMap;
+		return treeMap;
 	}
+	
+	private static Map<String, Long> createLinkedHashMap() {
+		LinkedHashMap<String, Long> linkedHashMap = new LinkedHashMap<>();
+		
+		populateMap(linkedHashMap);
+		
+		return linkedHashMap;
+	}
+	
+	private static void populateMap(Map<String, Long> map) {
+		for(int i=0;i<cities.length;i++)
+			map.put(cities[i],populations[i]);
+	}	
 
 }
